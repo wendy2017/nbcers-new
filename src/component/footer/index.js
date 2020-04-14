416639dced9048ab9da99d1e8432bbfdd229900c
zpf <13967587751@163.com>
2020-04-14 11:24:43 +0800

    备案号

--- src/component/footer/index.js
+++ src/component/footer/index.js
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom';
 const Footer = () => (
   <div className="footer-main">
     Copyright ©️ 2019 nbceres
-    <span className="footer-name"> 浙ICP备17038603号</span>
+    {/* <span className="footer-name"> 浙ICP备17038603号</span> */}
+    <a className="footer-name" href="http://www.beian.miit.gov.cn" target="_blank">浙ICP备17038603号</a>
   </div>
 );
 
